ff12519250b7e94fd558cac65166fc9c6c74f8f8
Jakhongirkhoja-Bakhodirov <masternuxtjstest@gmail.com>
2022-03-05 14:49:35 +0500

    simple factory

---
+++ design-patterns-with-kamranahmedse/creational-patterns/simple-factory.php
@@ -0,0 +1,43 @@
+<?php
+
+interface Door
+{
+    public function getWidth(): float;
+    public function getHeight(): float;
+}
+
+
+class WoodenDoor implements Door
+{
+    protected $width;
+    protected $height;
+    public function __construct(float $width, float $height)
+    {
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    public function getWidth(): float
+    {
+        return $this->width;
+    }
+
+    public function getHeight(): float
+    {
+        return $this->height;
+    }
+}
+
+
+class DoorFactory
+{
+    public static function makeOrder($width, $height): Door
+    {
+        return new WoodenDoor($width, $height);
+    }
+}
+
+$door = DoorFactory::makeOrder(100, 200);
+
+echo 'Width: ' . $door->getWidth();
+echo 'Height: ' . $door->getHeight();
